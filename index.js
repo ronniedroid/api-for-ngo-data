e65a3cc35524c55347f95e4ecfed2f8f27f63d9f
ronniedroid <ronni.n.yousif@gmail.com>
2022-10-27 18:43:00 +0300

    fixed a mistake

--- index.js
+++ index.js
@@ -146,8 +146,8 @@ app.get("/v2/dashboard/:year/:month", (req, res) => {
     const filteredCluster = cluster === "general" ? "" : cluster;
     return {
       [cluster]: {
-        total: tools.getBens(data, "total", filteredCluster),
-        gender: {
+        bens: {
+          total: tools.getBens(data, "total", filteredCluster),
           male: tools.getBens(data, "male", filteredCluster),
           female: tools.getBens(data, "female", filteredCluster),
         },
